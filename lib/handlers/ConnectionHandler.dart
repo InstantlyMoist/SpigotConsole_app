115097195196da1900e24ade86619099649b3fd4
InstantlyMoist <kyllian007@gmail.com>
2020-05-05 15:05:42 +0200

    Added initial handshake protocol

--- lib/handlers/ConnectionHandler.dart
+++ lib/handlers/ConnectionHandler.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:spigotconsole/models/server_model.dart';
 import 'package:web_socket_channel/io.dart';
@@ -6,17 +8,36 @@ class ConnectionHandler {
 
   static var connection;
 
-  static void connect(BuildContext context, ServerModel model, bool setup) {
-    connection = IOWebSocketChannel.connect(model.getUrl());
+  static void connect(BuildContext context, ServerModel model, bool setup) async {
+    connection = await IOWebSocketChannel.connect(model.getUrl());
+
+
     //TODO: Hash key and send it for key matching
-    if (setup) {
-      Navigator.pop(context);
-      connection.sink.add(model.key);
-    }
+
+    if (setup) Navigator.pop(context);
+    connection.sink.add(jsonEncode({
+      'type': 'INITIALHANDSHAKE',
+      'message': model.key
+    }));
     // If connection is ok, go to screen
 
-    connection.stream.listen((message) {
-      connection.sink.add("received!");
+    connection.stream.listen((message) async {
+      Map<String, dynamic> response = await jsonDecode(message);
+      String receivedType = response['type'];
+      String receivedMessage = response['message'];
+      switch (receivedType) {
+        case "INITIALHANDSHAKE":
+          if (receivedMessage == "OK") {
+            print("KEY OK");
+          } else {
+            print("error screen!!!!");
+          }
+      }
+
+
+
+      print(message);
+      //connection.sink.add("received!");
     });
   }
 
