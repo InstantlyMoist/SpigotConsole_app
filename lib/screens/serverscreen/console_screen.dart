92bd136501788cd633c6b3129fd3ec662fc5e34d
InstantlyMoist <kyllian007@gmail.com>
2020-05-08 14:37:25 +0200

    Added console command sending

--- lib/screens/serverscreen/console_screen.dart
+++ lib/screens/serverscreen/console_screen.dart
@@ -1,16 +1,17 @@
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:spigotconsole/handlers/ConnectionHandler.dart';
+import 'package:spigotconsole/screens/serverscreen/consolescreen/console_screen_input.dart';
 
 class ConsoleScreen extends StatefulWidget {
-
-
   @override
   _ConsoleScreenState createState() => _ConsoleScreenState();
 }
 
 class _ConsoleScreenState extends State<ConsoleScreen> {
+  final TextEditingController consoleController = new TextEditingController();
 
   String showingText = "This is the console start";
 
@@ -21,15 +22,41 @@ class _ConsoleScreenState extends State<ConsoleScreen> {
       setState(() {
         showingText += "\n${data['message']}";
       });
-      print("RECEIVED $data");
+      print(data);
+    });
+  }
+
+  void enter(String text) {
+    setState(() {
+      ConnectionHandler.connection.sink.add(
+          jsonEncode({'type': 'CONSOLE', 'message': consoleController.text}));
+      consoleController.text = "";
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: SingleChildScrollView(
-        child: Text(showingText),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              margin: EdgeInsets.all(20),
+              color: Color(0xFFF3F3F3),
+              child: SingleChildScrollView(
+                padding: EdgeInsets.all(10),
+                child: Text(showingText),
+              ),
+            ),
+          ),
+          ConsoleScreenInput(
+            controller: consoleController,
+            numeric: false,
+            hint: "Type here...",
+            enter: (text) => enter(text),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/screens/serverscreen/consolescreen/console_screen_input.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ConsoleScreenInput extends StatelessWidget {
+
+  final TextEditingController controller;
+  final String hint;
+  final bool numeric;
+  final Function(String) enter;
+
+  ConsoleScreenInput({this.controller, this.hint, this.numeric, this.enter});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(bottom: 20, left: 20, right: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Theme(
+            data: Theme.of(context).copyWith(
+              splashColor: Colors.transparent,
+            ),
+            child: TextField(
+              onSubmitted: (text) => enter(text),
+              controller: controller,
+              keyboardType: numeric ? TextInputType.number : TextInputType.text,
+              autofocus: false,
+              style: TextStyle(fontSize: 14, color: Colors.white),
+              cursorColor: Colors.orange,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Color(0xFFE07A05),
+                hintText: hint,
+                hintStyle: TextStyle(color: Colors.white.withOpacity(0.5)),
+                contentPadding:
+                const EdgeInsets.only(left: 14.0),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.transparent),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.transparent),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
