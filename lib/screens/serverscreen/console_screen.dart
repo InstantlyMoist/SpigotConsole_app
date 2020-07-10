f0bde9c3f5f04335bb94d71041ad2ab8b6455b25
InstantlyMoist <kyllian007@gmail.com>
2020-07-10 20:03:50 +0200

    added front-end color rendering

--- lib/screens/serverscreen/console_screen.dart
+++ lib/screens/serverscreen/console_screen.dart
@@ -13,17 +13,60 @@ class ConsoleScreen extends StatefulWidget {
 class _ConsoleScreenState extends State<ConsoleScreen> {
   final TextEditingController consoleController = new TextEditingController();
 
-  String showingText = "This is the console start";
+  RichText finalText;
+
+  List<TextSpan> log;
+
+  GlobalKey key = new GlobalKey();
 
   @override
   void initState() {
     super.initState();
+    log = new List();
     ConnectionHandler.messageStream.stream.listen((data) {
-      setState(() {
-        showingText += "\n${data['message']}";
-      });
-      print(data);
+      setState(
+        () {
+          for (String colorable in data['message'].split('§')) {
+            if (colorable.isNotEmpty) {
+              print(data['message'].split('§'));
+              String colorCode = colorable[0];
+              colorable = colorable.substring(1, colorable.length);
+              log.add(
+                TextSpan(
+                  text: colorable,
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: getColor(colorCode),
+                  ),
+                ),
+              );
+            }
+          }
+          log.add(TextSpan(text: "\n"));
+        },
+      );
     });
+  }
+
+  Color getColor(String colorCode) {
+    switch(colorCode) {
+      case "0": return Colors.black;
+      case "1": return Color(0xFF0000AA);
+      case "2": return Color(0xFF00AA00);
+      case "3": return Color(0xFF00AAAA);
+      case "4": return Color(0xFFAA0000);
+      case "5": return Color(0xFFAA00AA);
+      case "6": return Color(0xFFAA5500);
+      case "7": return Color(0xFFAAAAAA);
+      case "8": return Color(0xFF555555);
+      case "9": return Color(0xFF5555FF);
+      case "a": return Color(0xFF55FF55);
+      case "b": return Color(0xFF55FFFF);
+      case "c": return Color(0xFFFF5555);
+      case "d": return Color(0xFFFF55FF);
+      case "e": return Color(0xFFFFFF55);
+      case "f": return Colors.white;
+    }
   }
 
   void enter(String text) {
@@ -32,6 +75,14 @@ class _ConsoleScreenState extends State<ConsoleScreen> {
           jsonEncode({'type': 'CONSOLE', 'message': consoleController.text}));
       consoleController.text = "";
     });
+  }
+
+  RichText buildText() {
+    print("building text...");
+    print(log);
+    return RichText(
+      text: TextSpan(children: log.toList()),
+    );
   }
 
   @override
@@ -46,7 +97,8 @@ class _ConsoleScreenState extends State<ConsoleScreen> {
               color: Theme.of(context).accentColor,
               child: SingleChildScrollView(
                 padding: EdgeInsets.all(10),
-                child: Text(showingText),
+                child: buildText(),
+                //child: Text(showingText),
               ),
             ),
           ),
